0f0ec47639e1ac94e0a8a5e61202bfeff4f37b7c
ian35944 <99160345+ian35944@users.noreply.github.com>
2022-08-06 13:30:38 -0600

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+print("ingrese un numero positivo")
+n=int(input())
+if n<0:
+    print("“Los números negativos no son permitidos")
+elif n>0:
+    for i in range(n,-1,-1):
+         print (i, end=",")
+elif n==0:
+    print("el numero es 0")
